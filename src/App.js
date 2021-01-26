bf6365770e13af064b80a367a7bf1d704e699adf
Robert Gregg <hi@robertgregg.co.uk>
2021-01-26 13:43:03 +0000

    Added Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React from "react";
 import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
 import "./css/app.css";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* App body */}
-      {/* Sidebar */}
-      {/* Main Content - Feed */}
-      {/* Right Sidebar - widgets */}
+      <div className="app__body">
+        <Sidebar />
+        {/* Sidebar */}
+        {/* Main Content - Feed */}
+        {/* Right Sidebar - widgets */}
+      </div>
     </div>
   );
 }
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,8 +1,30 @@
 import React from "react";
+import SidebarRow from "./SidebarRow";
 import "../css/Sidebar.css";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import { ExpandMoreOutlined } from "@material-ui/icons";
 
 function Sidebar() {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <SidebarRow src="" title="Robert Gregg" />
+      <SidebarRow
+        Icon={LocalHospitalIcon}
+        title="COVOD-19 Information Center"
+      />
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow Icon={ChatIcon} title="Messenger" />
+      <SidebarRow Icon={StorefrontIcon} title="MarketPlace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+      <SidebarRow Icon={ExpandMoreOutlined} title="MarketPlace" />
+    </div>
+  );
 }
 
 export default Sidebar;
