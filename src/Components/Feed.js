695dd53b437df61e1d151fe4e3c267f988573e80
Robert Gregg <hi@robertgregg.co.uk>
2021-01-27 11:30:50 +0000

    Connected posts to the db

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../css/Feed.css";
 import MessageSender from "./MessageSender";
 import StoryReel from "./StoryReel";
 import Post from "./Post";
+import db from "../firebase";
 
 function Feed() {
+  const [posts, setposts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setposts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+  }, []);
+
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
-      <Post />
-      <Post />
-      <Post />
+
+      {posts.map((post) => (
+        <Post
+          key={post.data.id}
+          image={post.data.image}
+          message={post.data.message}
+          profilePic={post.data.profilePic}
+          username={post.data.username}
+          timestamp={post.data.timestamp}
+        />
+      ))}
     </div>
   );
 }
